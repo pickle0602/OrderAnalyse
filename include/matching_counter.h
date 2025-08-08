6c414f714b55f5119f693796c33ecd68b9dd65ba
WeiYiChen <ethan0602@163.com>
2025-08-08 17:07:16 +0800

    master

--- include/matching_counter.h
+++ include/matching_counter.h
@@ -13,7 +13,6 @@ namespace order_analyse {
 class MatchingCounter : public Calculator {
  private:
  public:
-<<<<<<< HEAD
   Sheet Calculate(const Sheet& sheet, const std::vector<std::string>& headers) {
     if (ArgumentsLimiter(headers.size())) {
       Sheet bad_sheet;
@@ -93,23 +92,6 @@ class MatchingCounter : public Calculator {
                                                row[h2_index]});
     }
     return result;
-=======
-  std::map<std::string, int> Calculate(const OrderList& order_list,
-                                       const std::vector<std::string> headers) {
-    if (arguments_limiter(headers.size())) {
-      std::map<std::string, int> error_map;
-      error_map["Error"] = -1;
-      return error_map;
-    }
-    size_t main_index = order_list.index(headers[0]);
-    size_t sub_index = order_list.index(headers[1]);
-    std::map<std::string, std::set<std::string>> main2set;
-    for (const auto& order : order_list.orders())
-      main2set[order.cell()[main_index]].insert(order.cell()[sub_index]);
-    std::map<std::string, int> main2size;
-    for (const auto& m2s : main2set) main2size[m2s.first] = m2s.second.size();
-    return main2size;
->>>>>>> origin/new-master
   }
   bool arguments_limiter(int vec_size) { return vec_size != 2; }
 };
